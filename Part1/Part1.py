4802569b963622c4cd0d0fb35a89591544fa45bd
Christiantk02 <143506438+Christiantk02@users.noreply.github.com>
2025-11-10 13:37:40 +0100

    Update Part1.py

--- Part1/Part1.py
+++ Part1/Part1.py
@@ -13,11 +13,8 @@ def white_noise(N, p_white):
     
 # Baseline drift noise
 def baseline_drift_noise(N, p_r, fs):
-    noise = np.zeros(N)
     white = white_noise(N, p_r)
-    for i in range(0, len(white)):
-        for j in range(0, i):
-            noise[i] += (white[j] * (1/fs))
+    noise = np.cumsum(white * (1/fs))
 
     return noise
         
@@ -30,21 +27,23 @@ def narrowband_noise(N, p_mains, fs):
     return A * np.cos(2 * np.pi * 50 * t + phi)
 
 # Add noise to ECG
-def add_noise(signal, fs, types=("white", "baseline", "narrowband"), powers=(0.01, 0.01, 0.01)):
+def add_noise(signal, fs, types=("white", "baseline", "narrowband"), factors=(0.01, 0.01, 0.01)):
     N = len(signal)
     noise = np.zeros(N)
     p = 0
 
-    if len(types) != len(powers):
+    p_signal = np.mean(signal**2)
+
+    if len(types) != len(factors):
         raise ValueError("Length of types and powers must be the same.")
 
     for t in types:
         if t == "white":
-            noise += white_noise(N, powers[p])
+            noise += white_noise(N, factors[p] * p_signal  )
         elif t == "baseline":
-            noise += baseline_drift_noise(N, powers[p], fs)
+            noise += baseline_drift_noise(N, factors[p] * p_signal, fs)
         elif t == "narrowband":
-            noise += narrowband_noise(N, powers[p], fs)
+            noise += narrowband_noise(N, factors[p] * p_signal, fs)
         else:
            raise ValueError(f"Unknown noise type: {t}")
         p += 1 
@@ -79,25 +78,25 @@ ecg = data['s'].flatten()
 fs = int(data['fs'].flatten()[0])
 
 # Add noise to the ECG signal
-ecg_noisy = add_noise(ecg, fs, types=("white", "baseline", "narrowband"), powers=(0.03, 0.01, 0.01))
+ecg_noisy = add_noise(ecg, fs, types=("white", "baseline", "narrowband"), factors=(1, 0.5, 0.5))
 
 # Get crossing intervals for both signals
-treshold = 0.6
+threshold = 0.6
 
-orginal_intervals = np.array(get_crossing_intervals(ecg, fs, threshold=treshold))*1000
-noisy_intervals = np.array(get_crossing_intervals(ecg_noisy, fs, threshold=treshold))*1000
+original_intervals = np.array(get_crossing_intervals(ecg, fs, threshold=threshold))*1000
+noisy_intervals = np.array(get_crossing_intervals(ecg_noisy, fs, threshold=threshold))*1000
 
-orginal_BPM = 60 / np.average(orginal_intervals) * 1000
-noisy_BPM = 60 / np.average(noisy_intervals) * 1000
+original_BPM = 60 / np.mean(original_intervals) * 1000
+noisy_BPM = 60 / np.mean(noisy_intervals) * 1000
 
 # Print the results
 print("Original ECG Signal:")
-print(f"Number of intervals: {len(orginal_intervals)}")
-print(f"Avrage BPM: {orginal_BPM}")
+print(f"Number of intervals: {len(original_intervals)}")
+print(f"Average BPM: {original_BPM}")
 
 print("\nNoisy ECG Signal:")
 print(f"Number of intervals: {len(noisy_intervals)}")
-print(f"Avrage BPM: {noisy_BPM}")
+print(f"Average BPM: {noisy_BPM}")
 
 # Plot the signals
 t = np.arange(len(ecg)) / fs
@@ -105,13 +104,13 @@ t = np.arange(len(ecg)) / fs
 plt.figure(figsize=(12, 6))
 plt.subplot(2, 1, 1)
 plt.plot(t, ecg, label='Original ECG')
-plt.axhline(y=treshold, color='red')
+plt.axhline(y=threshold, color='red')
 plt.title('Original ECG Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.subplot(2, 1, 2)
 plt.plot(t, ecg_noisy, label='Noisy ECG', color='orange')
-plt.axhline(y=treshold, color='red')
+plt.axhline(y=threshold, color='red')
 plt.title('Noisy ECG Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
